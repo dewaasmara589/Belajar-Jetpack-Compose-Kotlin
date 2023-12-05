0de08a17f755d134197ad9d6f172697b2d20e267
Dewa <dewa.a.putra@sat.co.id>
2023-12-05 15:57:31 +0700

    Complete make chat ui source from google documentary

--- app/src/main/java/com/alfastore/testjetpackcompose/ImageColumnRow.kt
+++ app/src/main/java/com/alfastore/testjetpackcompose/ImageColumnRow.kt
@@ -3,25 +3,34 @@ package com.alfastore.testjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -36,34 +45,25 @@ class ImageColumnRow : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent{
-            secondScreen()
-        }
-    }
-}
-
-@Composable
-fun secondScreen(){
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()
-    ) {
-        for (count in 1..10){
-            Card (modifier = Modifier
-                .padding(vertical = 10.dp, horizontal = 8.dp)
-                .background(Color.LightGray)
-                .fillMaxWidth(),
-                shape = RoundedCornerShape(0.dp)
-            ) {
-                content()
+            Surface(modifier = Modifier.fillMaxSize()) {
+                Content(Message("Name1", "Message TEXT1"))
             }
         }
     }
 }
 
+// Data Model
+data class Message(val title: String, val subTitle: String)
+
 @Composable
-fun content(){
+fun Content(msg: Message){
     Row (modifier = Modifier
-        .padding(vertical = 10.dp, horizontal = 8.dp)) {
+        .padding(vertical = 10.dp, horizontal = 8.dp)
+        .fillMaxHeight()
+        .fillMaxWidth()
+        .background(Color.LightGray)
+        .padding(all = 10.dp)
+    ) {
         Box(modifier = Modifier
             .size(40.dp)
             .clip(shape = CircleShape)
@@ -77,13 +77,24 @@ fun content(){
 
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
-            Text(text = "Name", fontWeight = FontWeight.Bold, fontSize = 16.sp, color = Color.Gray)
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        Column (modifier = Modifier.clickable { isExpanded = !isExpanded }){
+            Text(text = msg.title, fontWeight = FontWeight.Bold, fontSize = 16.sp, color = Color.Gray)
             Spacer(modifier = Modifier.height(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
                 Text(
-                    text = "Message Text",
+                    text = msg.subTitle,
                     modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.bodyMedium
                 )
             }
@@ -91,8 +102,37 @@ fun content(){
     }
 }
 
+@Composable
+fun Conversation(messages: List<Message>){
+    LazyColumn{
+        items(messages){ message ->
+            Content(message)
+            Divider(
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+                    .height(2.dp),
+                color = Color.Black
+            )
+        }
+    }
+}
+
+val messages = listOf(
+    Message("Student 1", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."),
+    Message("Student 2", "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    Message("Student 3", "15"),
+    Message("Student 4", "87"),
+    Message("Student 5", "22"),
+    Message("Student 6", "Where does it come from?"),
+    Message("Student 7", "125"),
+    Message("Student 8", "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."),
+    Message("Student 9", "15"),
+    Message("Student 10", "87"),
+    Message("Student 11", "22"),
+)
+
 @Preview(showBackground = true)
 @Composable
-fun previewDesign(){
-    secondScreen()
+fun PreviewDesign(){
+    Conversation(messages)
 }
